f1f7654ef9a0aebf79a4eefd9019492920413e17
Alan Smith <commit@alanwsmith.com>
2022-10-22 22:16:40 -0400

    Moved to a grimoire based source

--- site/autocomplete-select-menu/src/js.js
+++ site/autocomplete-select-menu/src/js.js
@@ -27,56 +27,78 @@ const itemList = [
     'Fira Sans',
 ]
 
+const activeList = []
+
 const updateItems = (filter) => {
     const itemWrapper = document.getElementById('menuItems')
     while (itemWrapper.children.length > 0) {
         itemWrapper.children[0].remove()
     }
-
-    const filteredItems = [...itemList]
     if (filter) {
-        filteredItems.length = 0
-        log(filter)
+        activeList.length = 0
         const pattern = new RegExp(filter, 'gi')
         itemList.forEach((item) => {
             const compareItem = item.toLowerCase()
             if (item.toLowerCase().match(pattern)) {
-                filteredItems.push(item)
+                activeList.push(item)
             }
         })
     }
-
-    const itemCount = Math.min(5, filteredItems.length)
+    const itemCount = Math.min(5, activeList.length)
     for (let i = 0; i < itemCount; i++) {
         const newItem = document.createElement('button')
         // TODO: Make this a valid key
-        newItem.id = `item--${filteredItems[i]}`
-        newItem.innerHTML = filteredItems[i]
+        newItem.id = `item--${activeList[i]}`
+        newItem.innerHTML = activeList[i]
         itemWrapper.appendChild(newItem)
     }
 }
 
-const log = (msg) => {
-    console.log(msg)
-}
-
 const handleMenuClick = (event) => {
-    log(event)
+    console.log(event)
 }
 
 const handleMenuInput = (event) => {
-    log(event)
-    updateItems(event.target.value)
+    console.log(event)
+    const textFilter = document
+        .getElementById('font-input-field')
+        .innerText.trim()
+    console.log(`-${textFilter}-`)
+    updateItems(textFilter)
+}
+
+const handleKeyUp = (event) => {
+    // TODO: Handle escape and tab
+    // TODO: Handle arrow keys
+    if (event.key.toLowerCase() === 'enter') {
+        console.log("Caught enter")
+        if (activeList[0]) {
+            console.log(`THING: ${activeList[0]}`)
+            document.getElementById('active-font').innerText= activeList[0]
+            document.getElementById('font-input-field').innerText = ''
+            activeList.length = 0
+            updateItems()
+        }
+        document.getElementById('font-input-field').innerText =
+           document.getElementById('font-input-field').innerText.replaceAll(/\n/g, '')
+        console.log(activeList[0])
+    }
 }
 
 const kickoff = () => {
-    log(`Kickoff: ${new Date().getTime()}`)
+    console.log(`Kickoff: ${new Date().getTime()}`)
+    // clear for refreshes during testing
+    // document.getElementById('font-input-field').innerText = ''
     document
         .getElementById('menuWrapper')
         .addEventListener('click', handleMenuClick)
     document
         .getElementById('menuWrapper')
         .addEventListener('input', handleMenuInput)
+
+    document
+        .getElementById('menuWrapper')
+        .addEventListener('keyup', handleKeyUp)
     updateItems()
 }
 
