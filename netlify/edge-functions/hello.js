924079aa11159a5b9d6bf0f7936522b049d0a104
Alan Smith <commit@alanwsmith.com>
2022-10-24 21:05:02 -0400

    Making the redirets files

--- netlify/edge-functions/hello.js
+++
@@ -1,9 +0,0 @@
-export default async (request, context) => {
-    let headerText = ''
-    request.headers.forEach((header) => {
-        headerText += `${header}\n`
-    })
-    return new Response(context.json, {
-        headers: { 'content-type': 'text/plain' },
-    })
-}
--- netlify/functions/recipe-redirects/index.js
+++ netlify/functions/recipe-redirects/index.js
@@ -1,6 +1,14 @@
 exports.handler = async function (event, context) {
+  const files = {
+  "2GbDpBAwq2AB": "/recipes/javascript-class-example--2GbDpBAwq2AB/index.html",
+  "2GbIL3X471Fa": "/recipes/dynamically-load-external-fonts--2GbIL3X471Fa/index.html",
+  "2GbISkLaYoEW": "/recipes/details-element-example--2GbISkLaYoEW/index.html",
+  "2GbIUkcEyxaH": "/recipes/autocomplete-select-menu--2GbIUkcEyxaH/index.html",
+  "2GbIaPbO7xXQ": "/recipes/detect-enter-key-in-input-field--2GbIaPbO7xXQ/index.html",
+  "2GbIh2R7rTEd": "/recipes/search-input-example--2GbIh2R7rTEd/index.html"
+}
   return {
     statusCode: 200,
-    body: JSON.stringify(event),
+    body: JSON.stringify(event.path),
   }
 }
