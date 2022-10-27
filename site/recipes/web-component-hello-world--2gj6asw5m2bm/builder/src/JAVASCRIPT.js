77161e51d23d11d437e6d25377d6377ef1e60edb
Alan Smith <commit@alanwsmith.com>
2022-10-27 19:25:44 -0400

    Tweak formatting

--- site/recipes/web-component-hello-world--2gj6asw5m2bm/builder/src/JAVASCRIPT.js
+++ site/recipes/web-component-hello-world--2gj6asw5m2bm/builder/src/JAVASCRIPT.js
@@ -1,9 +1,11 @@
 class HelloComponent extends HTMLElement {
     constructor() {
         super()
+
         this.attachShadow({ mode: 'open' })
         const wrapper = document.createElement('span')
         wrapper.innerText = 'Hello, Component!'
+
         this.shadowRoot.append(wrapper)
     }
 }
