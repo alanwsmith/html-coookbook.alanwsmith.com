bfa259b7a1dafe02492e930d0880433060d98855
Alan Smith <commit@alanwsmith.com>
2022-10-28 19:56:09 -0400

    Finished custom compoenent example code

--- site/recipes/web-components--create-and-send-event--2gmdsyscqjnv/builder/src/JAVASCRIPT.js
+++ site/recipes/web-components--create-and-send-event--2gmdsyscqjnv/builder/src/JAVASCRIPT.js
@@ -1,7 +1,10 @@
+///////////////////////////////////////////////////////
+// The Component
+//
 class EventExample extends HTMLElement {
     constructor() {
         super()
-
+        this.attachShadow({ mode: 'open' })
         // const event = new Event('build')
         // // Listen for the event.
         // this.addEventListener(
@@ -14,36 +17,54 @@ class EventExample extends HTMLElement {
         // // Dispatch the event.
         // this.dispatchEvent(event)
 
-        this.attachShadow({ mode: 'open' })
-        const wrapper = document.createElement('span')
-        wrapper.innerText = 'Hello, Component!'
+        // const wrapper = document.createElement('span')
+        // wrapper.innerText = 'Hello, Component!'
 
-        this.shadowRoot.append(wrapper)
+        const sendEvent = (event) => {
+            const componentEvent = new CustomEvent('componentActivated', {
+                detail: {
+                    value: new Date().getTime(),
+                },
+                composed: true,
+                bubbles: true,
+            })
+            this.dispatchEvent(componentEvent)
+        }
 
-        // const bbb = new Event('bbb')
-        const bbb = new CustomEvent('bbb', {
-            detail: 'this is the detail',
-            // cancelable: true,
-            composed: true,
-            bubbles: true,
-        })
+        const button = document.createElement('button')
+        button.addEventListener('click', sendEvent)
+        button.innerHTML = 'Click Here'
 
-        this.addEventListener('bbb', (e) => {
-            console.log(`caught locallly: ${e.detail}`)
-        })
-        // this.addEventListener('bbb', (e) => {}, false)
+        // () => {
 
-        setTimeout(() => {
-            console.log('timout triggered')
-            this.dispatchEvent(bbb)
-        }, 1000)
+        // // const bbb = new Event('bbb')
+        // const bbb = new CustomEvent('bbb', {
+        //     detail: 'this is the detail',
+        //     // cancelable: true,
+        //     composed: true,
+        //     bubbles: true,
+        // })
+        // this.addEventListener('bbb', (e) => {
+        //     console.log(`caught locallly: ${e.detail}`)
+        // })
+        // // this.addEventListener('bbb', (e) => {}, false)
+        // setTimeout(() => {
+        //     console.log('timout triggered')
+        //     this.dispatchEvent(bbb)
+        // }, 1000)
+
+        this.shadowRoot.append(button)
     }
 }
 
 customElements.define('event-example', EventExample)
 
-const hb = (event) => {
-    console.log(event)
+///////////////////////////////////////////////////////
+// Global
+
+const updateValue = (event) => {
+    const display = document.getElementById('display')
+    display.innerHTML = `New Value: ${event.detail.value}`
 }
 
-document.addEventListener('bbb', hb)
+document.addEventListener('componentActivated', updateValue)
