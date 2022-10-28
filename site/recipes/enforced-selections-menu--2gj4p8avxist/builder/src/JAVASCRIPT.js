d89e0773d322055a2d0f2c5bb951d234548bcb39
Alan Smith <commit@alanwsmith.com>
2022-10-27 22:57:15 -0400

    Catching the enter key and turned off spell check and auto complete

--- site/recipes/enforced-selections-menu--2gj4p8avxist/builder/src/JAVASCRIPT.js
+++ site/recipes/enforced-selections-menu--2gj4p8avxist/builder/src/JAVASCRIPT.js
@@ -18,6 +18,15 @@ class EnforcedSelector extends HTMLElement {
             renderOptions()
         }
 
+        const handleInputKeyup = (event) => {
+            const keyCheck = event.key.toLowerCase()
+            if (keyCheck === 'enter') {
+                registerSelection()
+            } else {
+                renderOptions()
+            }
+        }
+
         const loadDefaultOptions = () => {
             const defaultOptions = this.getElementsByTagName('option')
             for (let i = 0; i < defaultOptions.length; i++) {
@@ -26,6 +35,10 @@ class EnforcedSelector extends HTMLElement {
                     text: defaultOptions[i].innerText,
                 }
             }
+        }
+
+        const registerSelection = () => {
+            log('asdfasdfasfasdf')
         }
 
         const removeMenu = () => {
@@ -41,16 +54,24 @@ class EnforcedSelector extends HTMLElement {
 
         const renderOptions = () => {
             if (!this.select) {
-                // log('- Adding Menu')
                 this.select = document.createElement('select')
                 this.select.size = 5
             }
 
+            while (this.select.firstChild) {
+                this.select.firstChild.remove()
+            }
+
             for (const optionKey in this.defaultOptions) {
-                const option = document.createElement('option')
-                option.value = this.defaultOptions[optionKey].value
-                option.innerText = this.defaultOptions[optionKey].text
-                this.select.appendChild(option)
+                const defaultOption = this.defaultOptions[optionKey]
+                if (
+                    defaultOption.text.toLowerCase().includes(this.input.value)
+                ) {
+                    const option = document.createElement('option')
+                    option.value = this.defaultOptions[optionKey].value
+                    option.innerText = this.defaultOptions[optionKey].text
+                    this.select.appendChild(option)
+                }
             }
 
             this.wrapper.appendChild(this.select)
@@ -62,6 +83,9 @@ class EnforcedSelector extends HTMLElement {
         this.input = document.createElement('input')
         this.input.setAttribute('type', 'text')
         this.input.addEventListener('focus', handleInputFocus)
+        this.input.addEventListener('keyup', handleInputKeyup)
+        this.input.setAttribute('autocorrect', false)
+        this.input.setAttribute('spellcheck', false)
 
         this.wrapper.appendChild(this.input)
         this.shadowRoot.append(this.wrapper)
