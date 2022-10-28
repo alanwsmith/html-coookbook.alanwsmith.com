058768dbf96552171b6d35481645c1cf718319f0
Alan Smith <commit@alanwsmith.com>
2022-10-27 21:58:56 -0400

    Pulling in the options

--- site/recipes/enforced-selections-menu--2gj4p8avxist/builder/src/JAVASCRIPT.js
+++ site/recipes/enforced-selections-menu--2gj4p8avxist/builder/src/JAVASCRIPT.js
@@ -2,19 +2,46 @@ class EnforcedSelector extends HTMLElement {
     constructor() {
         super()
         this.attachShadow({ mode: 'open' })
+        this.options = []
+        this.defaultOptions = {}
+
+        const log = (msg) => {
+            console.log(msg)
+        }
 
         const handleDocumentClick = (event) => {
             if (event.target !== this) {
                 log('deactivating')
+                for (const option of this.options) {
+                    option.remove()
+                }
+                this.select.blur()
+                this.select.remove()
             }
         }
 
         const handleInputFocus = () => {
             log('focus')
+            this.select = document.createElement('select')
+            this.select.size = 5
+            this.options[0] = document.createElement('option')
+            this.options[0].value = 'asdf'
+            this.options[0].innerText = 'asdf'
+            for (const option of this.options) {
+                this.select.appendChild(option)
+            }
+            this.wrapper.appendChild(this.select)
         }
 
-        const log = (msg) => {
-            console.log(msg)
+        const loadDefaultOptions = () => {
+            const defaultOptions = this.getElementsByTagName('option')
+            for (let i = 0; i < defaultOptions.length; i++) {
+                this.defaultOptions[defaultOptions[i].value] = {
+                    index: i,
+                    text: defaultOptions[i].innerText,
+                }
+            }
+            // log(this.defaultOptions)
         }
 
         this.wrapper = document.createElement('div')
@@ -24,13 +51,11 @@ class EnforcedSelector extends HTMLElement {
         this.input.setAttribute('type', 'text')
         this.input.addEventListener('focus', handleInputFocus)
 
-        this.input.id = 'asdfasdf'
         this.wrapper.appendChild(this.input)
         this.shadowRoot.append(this.wrapper)
 
-        this.state = 'closed'
-
-        document.addEventListener('click', handleDocumentClick)
+        document.addEventListener('mousedown', handleDocumentClick)
+        document.addEventListener('DOMContentLoaded', loadDefaultOptions)
     }
 }
 
