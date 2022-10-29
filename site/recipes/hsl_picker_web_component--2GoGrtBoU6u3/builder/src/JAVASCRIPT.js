dd8399543eaf98fcb94befba6f705401c1cb2481
Alan Smith <commit@alanwsmith.com>
2022-10-29 10:35:58 -0400

    Basic sliders in place

--- site/recipes/hsl_picker_web_component--2GoGrtBoU6u3/builder/src/JAVASCRIPT.js
+++ site/recipes/hsl_picker_web_component--2GoGrtBoU6u3/builder/src/JAVASCRIPT.js
@@ -18,14 +18,30 @@ class hslaPicker extends HTMLElement {
             const hslValue = `hsla(${this.hueValue}, ${this.saturationValue}%, ${this.lightnessValue}%, ${this.alphaValue})`
             console.log(hslValue)
             display.style.backgroundColor = hslValue
+            updateHueDisplay()
+            updateSaturationDisplay()
         }
 
-        const display = document.createElement('div')
-        // display.style.backgroundColor = assembleHSL()
-        display.style.width = '100px'
-        display.style.height = '100px'
-        updateHSL()
-        // display.innerHTML = 'x'
+        const updateSaturationDisplay = () => {
+            this.saturationDisplay.style.background = `linear-gradient(
+            0.25turn,
+            hsl(${this.hueValue}, 0%, ${this.lightnessValue}%),
+            hsl(${this.hueValue}, 100%, ${this.lightnessValue}%))`
+        }
+
+        const updateHueDisplay = () => {
+            hueDisplay.style.background = `linear-gradient(
+            0.25turn,
+            hsl(0, 100%, 50%),
+            hsl(45, 100%, 50%),
+            hsl(90, 100%, 50%),
+            hsl(135, 100%, 50%),
+            hsl(180, 100%, 50%),
+            hsl(225, 100%, 50%),
+            hsl(270, 100%, 50%),
+            hsl(315, 100%, 50%),
+            hsl(360, 100%, 50%))`
+        }
 
         const handleHueInput = (event) => {
             console.log(event.target.value)
@@ -39,29 +55,93 @@ class hslaPicker extends HTMLElement {
             updateHSL()
         }
 
-        const hue = document.createElement('input')
-        hue.setAttribute('id', 'hue-slider')
-        hue.setAttribute('name', 'hue-slider')
-        hue.setAttribute('type', 'range')
-        hue.setAttribute('min', '0')
-        hue.setAttribute('max', '360')
-        hue.setAttribute('size', '190')
-        hue.style.width = '200px'
-        hue.addEventListener('input', handleHueInput)
+        const handleLightnessInput = (event) => {
+            console.log(event.target.value)
+            this.lightnessValue = event.target.value
+            updateHSL()
+        }
 
-        const saturation = document.createElement('input')
-        saturation.setAttribute('id', 'hue-slider')
-        saturation.setAttribute('name', 'hue-slider')
-        saturation.setAttribute('type', 'range')
-        saturation.setAttribute('min', '0')
-        saturation.setAttribute('max', '100')
-        saturation.setAttribute('size', '190')
-        saturation.style.width = '200px'
-        saturation.addEventListener('input', handleSaturationInput)
+        const display = document.createElement('div')
+        display.style.width = '200px'
+        display.style.height = '30px'
+        display.style.border = '2px solid black'
+
+        const hueDiv = document.createElement('div')
+        // hueDiv.style.position = 'relative'
+
+        const hueSlider = document.createElement('input')
+        hueSlider.setAttribute('id', 'hue-slider')
+        hueSlider.setAttribute('name', 'hue-slider')
+        hueSlider.setAttribute('type', 'range')
+        hueSlider.setAttribute('min', '0')
+        hueSlider.setAttribute('max', '360')
+        hueSlider.setAttribute('size', '200')
+        hueSlider.style.width = '200px'
+        // hueSlider.style.position = 'absolute'
+        hueSlider.style.padding = '0'
+        hueSlider.style.margin = '0'
+        hueSlider.addEventListener('input', handleHueInput)
+
+        const hueDisplay = document.createElement('div')
+        hueDisplay.style.width = '180px'
+        hueDisplay.style.height = '18px'
+        hueDisplay.style.padding = '0'
+        hueDisplay.style.margin = '0'
+        // hueDisplay.style.position = 'absolute'
+        hueDisplay.style.left = '10px'
+
+        // background: linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),
+        // linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%),
+        // linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%);
+        // background: linear-gradient(#e66465, #9198e5);
+        // background: linear-gradient(0.25turn, #3f87a6, #ebf8e1, #f69d3c);
+
+        this.saturationDiv = document.createElement('div')
+        // this.saturationDiv.style.poisition = 'relative'
+
+        this.saturationDisplay = document.createElement('div')
+        this.saturationDisplay.style.width = '180px'
+        this.saturationDisplay.style.height = '18px'
+        this.saturationDisplay.style.padding = '0'
+        this.saturationDisplay.style.margin = '0'
+        // this.saturationDisplay.style.position = 'absolute'
+        this.saturationDisplay.style.left = '10px'
+
+        this.saturationSlider = document.createElement('input')
+        this.saturationSlider.setAttribute('id', 'saturation-slider')
+        this.saturationSlider.setAttribute('name', 'saturation-slider')
+        this.saturationSlider.setAttribute('type', 'range')
+        this.saturationSlider.setAttribute('min', '0')
+        this.saturationSlider.setAttribute('max', '100')
+        this.saturationSlider.setAttribute('size', '190')
+        this.saturationSlider.style.width = '200px'
+        this.saturationSlider.addEventListener('input', handleSaturationInput)
+
+        this.saturationDiv.appendChild(this.saturationDisplay)
+        this.saturationDiv.appendChild(this.saturationSlider)
+
+        this.lightnessDiv = document.createElement('div')
+
+        this.lightnessSlider = document.createElement('input')
+        this.lightnessSlider.setAttribute('id', 'lightness-slider')
+        this.lightnessSlider.setAttribute('name', 'lightness-slider')
+        this.lightnessSlider.setAttribute('type', 'range')
+        this.lightnessSlider.setAttribute('min', '0')
+        this.lightnessSlider.setAttribute('max', '100')
+        this.lightnessSlider.setAttribute('size', '190')
+        this.lightnessSlider.style.width = '200px'
+        this.lightnessSlider.addEventListener('input', handleLightnessInput)
+
+        this.lightnessDiv.appendChild(this.lightnessSlider)
 
         this.shadowRoot.append(display)
-        this.shadowRoot.append(hue)
-        this.shadowRoot.append(saturation)
+        this.shadowRoot.append(hueDisplay)
+        this.shadowRoot.append(hueSlider)
+
+        updateHSL()
+        this.shadowRoot.append(this.saturationDiv)
+        this.shadowRoot.append(this.lightnessDiv)
+        // this.shadowRoot.append(lightness)
     }
 }
 
