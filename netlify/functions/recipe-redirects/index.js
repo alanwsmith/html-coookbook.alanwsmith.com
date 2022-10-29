a03699fa6ac5b7b40499aa866df13201b27475fb
Alan Smith <commit@alanwsmith.com>
2022-10-29 00:49:52 -0400

    Starting anothe rrename

--- netlify/functions/recipe-redirects/index.js
+++ netlify/functions/recipe-redirects/index.js
@@ -9,23 +9,23 @@ const fire_redirect = (location) => {
 
 exports.handler = async function (event, context) {
   const files = {
-  "2gbdpbawq2ab": "/recipes/Javascript_class_example---2gbdpbawq2ab/index.html",
-  "2gbiapbo7xxq": "/recipes/Detect_enter_keypress_in_an_input_field---2gbiapbo7xxq/index.html",
-  "2gbih2r7rted": "/recipes/Search_input_example---2gbih2r7rted/index.html",
-  "2gbil3x471fa": "/recipes/Dynamically_load_external_fonts---2gbil3x471fa/index.html",
-  "2gbisklayoew": "/recipes/Details_element_example---2gbisklayoew/index.html",
-  "2gbiukceyxah": "/recipes/Autocomplete_select_menu---2gbiukceyxah/index.html",
-  "2gc2v5bvvngh": "/recipes/Font_download_test---2gc2v5bvvngh/index.html",
-  "2ggjxa4tvm9x": "/recipes/M-Dash_input_select_autocomplete_test---2ggjxa4tvm9x/index.html",
-  "2gj4p8avxist": "/recipes/Strict_selection_menu_form_control---2gj4p8avxist/index.html",
-  "2gj6asw5m2bm": "/recipes/Web_Components_hello_world---2gj6asw5m2bm/index.html",
-  "2gja3c8lgfbs": "/recipes/Web_Components_document_onclick_listener---2gja3c8lgfbs/index.html",
-  "2gjuqnwy1dko": "/recipes/Web_Components_use_internal_elements---2gjuqnwy1dko/index.html",
-  "2glvtfmqasay": "/recipes/CSS_absolute_and_relative_positioning---2glvtfmqasay/index.html",
-  "2gmdsyscqjnv": "/recipes/Web_Components_create_and_send_events---2gmdsyscqjnv/index.html",
-  "2gmvxjjsmlcb": "/recipes/Web_Components_custom_input_element---2gmvxjjsmlcb/index.html",
-  "2gn2p9a4zdjr": "/recipes/Web_Components_include_value_on_form_submit---2gn2p9a4zdjr/index.html",
-  "2gn6720koljv": "/recipes/Prevent_a_form_from_submitting_to_view_data---2gn6720koljv/index.html"
+  "2gbdpbawq2ab": "/recipes/jjavascript_class_example---2gbdpbawq2ab/index.html",
+  "2gbiapbo7xxq": "/recipes/ddetect_enter_keypress_in_an_input_field---2gbiapbo7xxq/index.html",
+  "2gbih2r7rted": "/recipes/ssearch_input_example---2gbih2r7rted/index.html",
+  "2gbil3x471fa": "/recipes/ddynamically_load_external_fonts---2gbil3x471fa/index.html",
+  "2gbisklayoew": "/recipes/ddetails_element_example---2gbisklayoew/index.html",
+  "2gbiukceyxah": "/recipes/aautocomplete_select_menu---2gbiukceyxah/index.html",
+  "2gc2v5bvvngh": "/recipes/ffont_download_test---2gc2v5bvvngh/index.html",
+  "2ggjxa4tvm9x": "/recipes/mm-dash_input_select_autocomplete_test---2ggjxa4tvm9x/index.html",
+  "2gj4p8avxist": "/recipes/sstrict_selection_menu_form_control---2gj4p8avxist/index.html",
+  "2gj6asw5m2bm": "/recipes/hhello_world_web_components_example---2gj6asw5m2bm/index.html",
+  "2gja3c8lgfbs": "/recipes/ddocument_onclick_listener_for_web_components---2gja3c8lgfbs/index.html",
+  "2gjuqnwy1dko": "/recipes/uuse_internal_elements_in_a_web_component---2gjuqnwy1dko/index.html",
+  "2glvtfmqasay": "/recipes/ccss_absolute_and_relative_positioning---2glvtfmqasay/index.html",
+  "2gmdsyscqjnv": "/recipes/ccreate_and_send_events_in_a_web_component---2gmdsyscqjnv/index.html",
+  "2gmvxjjsmlcb": "/recipes/ccreate_a_custom_input_element_in_a_web_component---2gmvxjjsmlcb/index.html",
+  "2gn2p9a4zdjr": "/recipes/ssend_value_with_web_component_form_submit---2gn2p9a4zdjr/index.html",
+  "2gn6720koljv": "/recipes/pprevent_a_form_from_submitting_to_view_data---2gn6720koljv/index.html"
 }
   const url_parts = event.path.split('/')
   // console.log(url_parts)
@@ -34,7 +34,7 @@ exports.handler = async function (event, context) {
     return fire_redirect('/')
   } else {
     // console.log('2')
-    const slug_parts = url_parts[2].split('---')
+    const slug_parts = url_parts[2].split('--')
     if (slug_parts.length !== 2) {
       // console.log('3')
       return fire_redirect('/')
