dcef105f9ca31d4ea692ccc0d8a17d314c2dc42a
Alan Smith <commit@alanwsmith.com>
2022-10-29 00:04:52 -0400

    Updated note

--- netlify/functions/recipe-redirects/index.js
+++ netlify/functions/recipe-redirects/index.js
@@ -9,23 +9,23 @@ const fire_redirect = (location) => {
 
 exports.handler = async function (event, context) {
   const files = {
-  "2gbdpbawq2ab": "/recipes/javascript-class-example--2gbdpbawq2ab/index.html",
-  "2gbiapbo7xxq": "/recipes/detect-enter-key-in-input-field--2gbiapbo7xxq/index.html",
-  "2gbih2r7rted": "/recipes/search-input-example--2gbih2r7rted/index.html",
+  "2gbdpbawq2ab": "/recipes/Javascript-Class-Example--2gbdpbawq2ab/index.html",
+  "2gbiapbo7xxq": "/recipes/Detect-Enter-Key-In-Input-Field--2gbiapbo7xxq/index.html",
+  "2gbih2r7rted": "/recipes/Search-Input-Example--2gbih2r7rted/index.html",
   "2gbil3x471fa": "/recipes/Dynamically-Load-External-Fonts--2gbil3x471fa/index.html",
-  "2gbisklayoew": "/recipes/details-element-example--2gbisklayoew/index.html",
-  "2gbiukceyxah": "/recipes/autocomplete-select-menu--2gbiukceyxah/index.html",
-  "2gc2v5bvvngh": "/recipes/font-download-test--2gc2v5bvvngh/index.html",
-  "2ggjxa4tvm9x": "/recipes/mdash-input-select-autocomplete-test--2ggjxa4tvm9x/index.html",
-  "2gj4p8avxist": "/recipes/strict-selection-menu-form-control--2gj4p8avxist/index.html",
+  "2gbisklayoew": "/recipes/Details-Element-Example--2gbisklayoew/index.html",
+  "2gbiukceyxah": "/recipes/Autocomplete-Select-Menu--2gbiukceyxah/index.html",
+  "2gc2v5bvvngh": "/recipes/Font-Download-Test--2gc2v5bvvngh/index.html",
+  "2ggjxa4tvm9x": "/recipes/M-Dash-Input-Select-Autocomplete-Test--2ggjxa4tvm9x/index.html",
+  "2gj4p8avxist": "/recipes/Strict-Selection-Menu-Form-Control--2gj4p8avxist/index.html",
   "2gn6720koljv": "/recipes/Prevent-A-Form-From-Submitting-To-View-Data--2gn6720koljv/index.html",
+  "Absolute-And-Relative-Positioning": "/recipes/CSS--Absolute-And-Relative-Positioning--2glvtfmqasay/index.html",
+  "Create-and-Send-Event": "/recipes/Web-Components--Create-and-Send-Event--2gmdsyscqjnv/index.html",
   "Custom-Input-Element": "/recipes/Web-Component--Custom-Input-Element--2gmvxjjsmlcb/index.html",
+  "Document-Click-Listener": "/recipes/Web-Components--Document-Click-Listener--2gja3c8lgfbs/index.html",
   "Hello-World": "/recipes/Web-Components--Hello-World--2gj6asw5m2bm/index.html",
   "Include-Value-On-Form-Submit": "/recipes/Web-Component--Include-Value-On-Form-Submit--2gn2p9a4zdjr/index.html",
-  "absolute-and-relative-positions": "/recipes/css--absolute-and-relative-positions--2glvtfmqasay/index.html",
-  "create-and-send-event": "/recipes/web-components--create-and-send-event--2gmdsyscqjnv/index.html",
-  "document-click-listener": "/recipes/web-components--document-click-listener--2gja3c8lgfbs/index.html",
-  "use-internal-elements": "/recipes/web-components--use-internal-elements--2gjuqnwy1dko/index.html"
+  "Use-Internal-Elements": "/recipes/Web-Components--Use-Internal-Elements--2gjuqnwy1dko/index.html"
 }
   const url_parts = event.path.split('/')
   // console.log(url_parts)
