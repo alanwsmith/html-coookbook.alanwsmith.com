78a485c81f0be4c9dec2709375f6fb8e6effc095
Alan Smith <commit@alanwsmith.com>
2022-12-02 23:12:59 -0500

    Fixed home page and added note

--- site/recipes/back_button_with_history_push_state--2imrt72t8sdv/builder/build.py
+++ site/recipes/back_button_with_history_push_state--2imrt72t8sdv/builder/build.py
@@ -13,13 +13,13 @@ class Builder():
         self.script_dir = sys.path[0]
         self.source_dir = os.path.join(self.script_dir, 'src')
         self.template_file = os.path.join(self.source_dir, 'TEMPLATE.html')
-        self.index_file = os.path.join(self.script_dir, '..', 'index-build-in-progress.html')
+        self.home_page = os.path.join(self.script_dir, '..', 'index.html')
         self.html_dir = os.path.join(self.script_dir, '..')
 
-    def make_index_page(self):
-        with open(self.template_file) as _in:
-            with open(self.index_file, 'w') as _index:
-                _index.write(_in.read())
+    # def make_index_page(self):
+    #     with open(self.template_file) as _in:
+    #         with open(self.index_file, 'w') as _index:
+    #             _index.write(_in.read())
 
     def make_individual_pages(self):
 
@@ -34,7 +34,6 @@ class Builder():
                 "DESCRIPTION": "Getting the back button to work with history.pushState()",
                 }
 
-
         with open(self.template_file) as _in:
             skeleton = Template(_in.read())
             for i in range(-10,10):
@@ -44,15 +43,19 @@ class Builder():
                 data['BODY_CODE'] = f"""<pre><code class="language-html">{escape(self.body_raw)}</code></pre>"""
                 data['JS_CODE'] = f"""<pre><code class="language-js">{escape(self.js_raw)}</code></pre>"""
 
-
                 output_path = os.path.join(self.html_dir, f"{i}.html")
                 with open(output_path, 'w') as _out:
                     _out.write(skeleton.substitute(data))
+
+                if i == 0:
+                    with open(self.home_page, 'w') as _out:
+                        _out.write(skeleton.substitute(data))
+
 
 
 if __name__ == '__main__':
     print(f"Building {datetime.now()}")
     b = Builder()
-    b.make_index_page()
+    # b.make_index_page()
     b.make_individual_pages()
 
