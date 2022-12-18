92ec2760e8689f703981f69cc3416c9bc50b6fb4
Alan Smith <commit@alanwsmith.com>
2022-12-18 00:10:51 -0500

    Removed videos to another repository

--- site/recipes/ffmpeg_webp_samples_2j1ofgslrwmd/builder/src/JAVASCRIPT.js
+++ site/recipes/ffmpeg_webp_samples_2j1ofgslrwmd/builder/src/JAVASCRIPT.js
@@ -1,36 +1,47 @@
 const images = {
-    alfa: { img: new Image() },
-    bravo: { img: new Image() },
-    charlie: { img: new Image() },
-    delta: { img: new Image() },
-    echo: { img: new Image() },
-    foxtrot: { img: new Image() },
-    golf: { img: new Image() },
-    hotel: { img: new Image() },
-    india: { img: new Image() },
+    raw: [],
+    baseline: [],
+    c0: [],
+    c1: [],
+    c2: [],
+    c3: [],
+    c4: [],
+    c5: [],
+    c6: [],
 }
 
 let loaded_images = 0
 
 const checkLoaded = () => {
     loaded_images += 1
-    if (loaded_images == 2) {
+    if (loaded_images == 9) {
         showImages()
     }
 }
 
 const showImages = () => {
     console.log('showing')
-    document.getElementById('alfa_container').appendChild(images.alfa.img)
-    document.getElementById('bravo_container').appendChild(images.bravo.img)
+    for (let image in images) {
+        for (let i = 0; i < image.length; i++) {
+            const id = `${image}${i}`
+            console.log(id)
+            const el = document.getElementById(`${image}${i}`)
+            if (el) {
+                el.appendChild(images[image][i])
+            }
+        }
+    }
 }
 
 const init = () => {
     console.log('init')
-    images.alfa.img.src = 'samples/alfa.webp'
-    images.bravo.img.src = 'samples/bravo.webp'
-    images.alfa.img.addEventListener('load', checkLoaded)
-    images.bravo.img.addEventListener('load', checkLoaded)
+    for (let name in images) {
+        for (let i = 0; i < 4; i++) {
+            images[name][i] = new Image()
+            images[name][i].src = `samples/${name}.webp`
+            images[name][i].addEventListener('load', checkLoaded)
+        }
+    }
 }
 
 document.addEventListener('DOMContentLoaded', init)
