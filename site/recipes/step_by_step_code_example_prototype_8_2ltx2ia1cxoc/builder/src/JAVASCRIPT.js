9d5ee6453eb2ed29782871bc47b54baee1ca84fd
Alan Smith <commit@alanwsmith.com>
2023-02-08 17:47:36 -0500

    workingon prototype 8

--- site/recipes/step_by_step_code_example_prototype_8_2ltx2ia1cxoc/builder/src/JAVASCRIPT.js
+++ site/recipes/step_by_step_code_example_prototype_8_2ltx2ia1cxoc/builder/src/JAVASCRIPT.js
@@ -1,5 +1,27 @@
+const config = {
+  source: `use std::env; 
+ 
+fn main() { 
+  let envVarResult = env::var("HOME"); 
+  match envVarResult { 
+    Ok(item) => { 
+      println!("got {}", item); 
+    } 
+    Err(error) => { 
+      println!("error {}", error); 
+    } 
+  } 
+}`,
+
+  sets: [
+    {
+      lines: [1],
+    },
+  ],
+}
+
 const init = () => {
-    console.log("init")
+  console.log('init')
 }
 
 document.addEventListener('DOMContentLoaded', init)
