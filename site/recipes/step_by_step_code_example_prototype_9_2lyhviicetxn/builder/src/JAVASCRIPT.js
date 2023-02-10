3079d83e815de6b09fb157d8d23a3c293aa62bc6
Alan Smith <commit@alanwsmith.com>
2023-02-10 12:52:06 -0500

    Getting turning off highlighting to work

--- site/recipes/step_by_step_code_example_prototype_9_2lyhviicetxn/builder/src/JAVASCRIPT.js
+++ site/recipes/step_by_step_code_example_prototype_9_2lyhviicetxn/builder/src/JAVASCRIPT.js
@@ -18,6 +18,39 @@ const loadLines = () => {
   s.lines = s.source.split('\n')
 }
 
+const removeHighlights = () => {
+  console.log('removeHighlights')
+  s.styleOverride = document.createElement('style')
+  s.styleOverride.innerHTML = `
+.ace-monokai .ace_line:not(:nth-child(2)) .ace_keyword,
+.ace-monokai .ace_line:not(:nth-child(2)) .ace_lparen,
+.ace-monokai .ace_line:not(:nth-child(2)) .ace_rparen,
+.ace-monokai .ace_line:not(:nth-child(2)) .ace_entity,
+.ace-monokai .ace_line:not(:nth-child(2)) .ace_name,
+.ace-monokai .ace_line:not(:nth-child(2)) .ace_function,
+.ace-monokai .ace_line:not(:nth-child(2)) .ace_source,
+.ace-monokai .ace_line:not(:nth-child(2)) .ace_rust
+
+{
+  color: #777;
+}
+`
+
+  document.body.appendChild(s.styleOverride)
+}
+
+/*
+const removeHighlights = () => {
+  console.log('removeHighlights')
+  // const els = document.getElementsByClassName('language-rust')
+  const els = document.getElementsByClassName('ace_line')
+  for (let i = 0; i <= els.length; i++) {
+    console.log(els[i])
+    // els[i].styles.classList.remove('ace_keyword')
+  }
+}
+*/
+
 const updateContent = (setIndex) => {
   s.setIndex = setIndex
   parts = []
@@ -32,7 +65,8 @@ const init = () => {
   s.editor = ace.edit('editor')
   s.editor.setTheme('ace/theme/monokai')
   s.editor.session.setMode('ace/mode/rust')
-  updateContent(1)
+  // updateContent(1)
+  removeHighlights()
 }
 
 document.addEventListener('DOMContentLoaded', init)
