553bbfa6b39eea903cbd7c7e6f56cd4cb01a22fd
Alan Smith <commit@alanwsmith.com>
2023-02-10 15:07:44 -0500

    Getting de-highlighter to work

--- site/recipes/step_by_step_code_example_prototype_9_2lyhviicetxn/builder/src/CONFIG.js
+++ site/recipes/step_by_step_code_example_prototype_9_2lyhviicetxn/builder/src/CONFIG.js
@@ -2,16 +2,24 @@
 // and data that should be held outside
 // of the main script
 
-const s = {
-  source: `fn main() {
-  println!("Hello, World");
+const c = {
+  source: `fn main() { 
+  let alfa = String::from("apple"); 
+  let charlie = widget(alfa); 
+  println!("charlie is {charlie}"); 
+} 
+ 
+fn widget(thing: String) -> String { 
+  println!("widget got {thing}"); 
+  let bravo = String::from("berry"); 
+  bravo 
 }`,
   sets: [
     {
       lines: [1],
     },
     {
-      lines: [1, 2],
+      lines: [3],
     },
   ],
 }
--- site/recipes/step_by_step_code_example_prototype_9_2lyhviicetxn/builder/src/JAVASCRIPT.js
+++ site/recipes/step_by_step_code_example_prototype_9_2lyhviicetxn/builder/src/JAVASCRIPT.js
@@ -1,71 +1,93 @@
-/*
-let editor
-
-
-
-
-const init = () => {
-  console.log('init')
-  loadLines()
-  editor = window.stepByStepContent
-  updateContent(1)
-  console.log(s)
-}
-
-*/
-
 const loadLines = () => {
-  s.lines = s.source.split('\n')
+  c.lines = c.source.split('\n')
 }
 
 const removeHighlights = () => {
   console.log('removeHighlights')
-  s.styleOverride = document.createElement('style')
-  s.styleOverride.innerHTML = `
-.ace-monokai .ace_line:not(:nth-child(2)) .ace_keyword,
-.ace-monokai .ace_line:not(:nth-child(2)) .ace_lparen,
-.ace-monokai .ace_line:not(:nth-child(2)) .ace_rparen,
-.ace-monokai .ace_line:not(:nth-child(2)) .ace_entity,
-.ace-monokai .ace_line:not(:nth-child(2)) .ace_name,
-.ace-monokai .ace_line:not(:nth-child(2)) .ace_function,
-.ace-monokai .ace_line:not(:nth-child(2)) .ace_source,
-.ace-monokai .ace_line:not(:nth-child(2)) .ace_rust
 
-{
+  const selectors = ['.ace_keyword', '.ace_lparen', '.ace_rparen']
+
+  console.log(c.lines.length)
+
+  const removers = []
+
+  for (let i = 0; i < c.lines.length; i++) {
+    const lineNumber = i + 1
+    if (!c.sets[c.set].lines.includes(lineNumber)) {
+      for (let x = 0; x < selectors.length; x++) {
+        removers.push(
+          `.ace-monokai .ace_line:nth-child(${lineNumber}) ${selectors[x]}`
+        )
+        console.log(selectors[x])
+      }
+    }
+  }
+
+  // console.log(removers)
+
+  c.styleOverride.innerHTML = `
+  .ace-monokai .ace_line:nth-child(2) .ace_lparen
+   {
+   color: #777;
+   }
+  `
+
+  c.styleOverride.innerHTML = `
+  .ace-monokai .ace_line:nth-child(2) .ace_rparen
+   {
+   color: #777;
+   }
+  `
+
+  // .ace-monokai .ace_line:nth-child(2) .ace_keyword,
+  // .ace-monokai .ace_line:nth-child(2) .ace_lparen,
+  // .ace-monokai .ace_line:nth-child(2) .ace_rparen,
+  // .ace-monokai .ace_line:nth-child(2) .ace_entity,
+  // .ace-monokai .ace_line:nth-child(2) .ace_name,
+  // .ace-monokai .ace_line:nth-child(2) .ace_function,
+  // .ace-monokai .ace_line:nth-child(2) .ace_source,
+  // .ace-monokai .ace_line:nth-child(2) .ace_rust
+  // {
+  // color: #777;
+  // }
+  // `
+
+  c.styleOverride.innerHTML = `
+  .ace-monokai .ace_line:nth-child(2) .ace_keyword
+  {
   color: #777;
-}
-`
-
-  document.body.appendChild(s.styleOverride)
-}
-
-/*
-const removeHighlights = () => {
-  console.log('removeHighlights')
-  // const els = document.getElementsByClassName('language-rust')
-  const els = document.getElementsByClassName('ace_line')
-  for (let i = 0; i <= els.length; i++) {
-    console.log(els[i])
-    // els[i].styles.classList.remove('ace_keyword')
   }
-}
-*/
+  `
 
-const updateContent = (setIndex) => {
-  s.setIndex = setIndex
+  // c.styleOverride.innerHTML = `.ace-monokai .ace_line:nth-child(2) .ace_keyword { color: #777; }`
+
+  const removers2 = [`.ace-monokai .ace_line:nth-child(2) .ace_keyword`]
+
+  c.styleOverride.innerHTML = `${removers.join(', ')} {color: #777;}`
+
+  // c.styleOverride.innerHTML = `.ace-monokai .ace_line:nth-child(2) .ace_keyword { color #777; }`
+  // c.styleOverride.innerHTML = `.ace-monokai .ace_line:nth-child(2) .ace_keyword { color #777; }`
+
+  console.log(c.styleOverride.innerHTML)
+  document.body.appendChild(c.styleOverride)
+}
+
+const updateContent = (set) => {
+  c.set = set
   parts = []
-  for (let i = 0; i <= s.setIndex; i++) {
-    parts.push(s.lines[i])
+  for (let i = 0; i <= c.lines.length; i++) {
+    parts.push(c.lines[i])
   }
-  s.editor.setValue(parts.join('\n'), 1)
+  c.editor.setValue(parts.join('\n'), 1)
 }
 
 const init = () => {
   loadLines()
-  s.editor = ace.edit('editor')
-  s.editor.setTheme('ace/theme/monokai')
-  s.editor.session.setMode('ace/mode/rust')
-  // updateContent(1)
+  c.editor = ace.edit('editor')
+  c.editor.setTheme('ace/theme/monokai')
+  c.editor.session.setMode('ace/mode/rust')
+  c.styleOverride = document.createElement('style')
+  updateContent(1)
   removeHighlights()
 }
 
