d4a1dcdbba9c25f9dd84bd6c72b47d561e620b42
Alan Smith <commit@alanwsmith.com>
2023-02-11 18:41:57 -0500

    Resizing and main movement in place

--- site/recipes/step_by_step_code_example_prototype_v12_scrolling_2lbq1ufz2bpv/builder/src/JAVASCRIPT.js
+++ site/recipes/step_by_step_code_example_prototype_v12_scrolling_2lbq1ufz2bpv/builder/src/JAVASCRIPT.js
@@ -1,66 +1,36 @@
-// const multiplier = 0.7
-
-function handleIntersect2(entries, observer) {
-  entries.forEach((entry) => {
-    const id = entry.target.id
-    const clientTop = entry.boundingClientRect.top
-    const rootTop = entry.rootBounds.top
-    const rootBottom = entry.rootBounds.bottom
-    const clientBottom = entry.boundingClientRect.bottom
-
-    if (clientTop < rootTop && clientBottom > rootTop) {
-      window.currentId.innerHTML = entry.target.id
-      console.log(
-        `-- down - ${id} - ${clientTop} - ${rootTop} - ${clientBottom}`
-      )
-    }
-  })
-}
-
 function handleIntersect1(entries, observer) {
   entries.forEach((entry) => {
     const id = entry.target.id
     const clientTop = entry.boundingClientRect.top
     const rootTop = entry.rootBounds.top
     const clientBottom = entry.boundingClientRect.bottom
+    if (clientTop < rootTop && clientBottom > rootTop) {
+      window.currentId.innerHTML = entry.target.id
+    }
+  })
+}
 
+function handleIntersect2(entries, observer) {
+  entries.forEach((entry) => {
+    const id = entry.target.id
+    const clientTop = entry.boundingClientRect.top
+    const rootTop = entry.rootBounds.top
     if (clientTop < rootTop) {
       window.currentId.innerHTML = entry.target.id
-      console.log(`-- up - ${id} - ${clientTop} - ${rootTop} - ${clientBottom}`)
-      // } else if (clientTop < rootTop && clientBottom > rootTop) {
-      // console.log(`-- dn - ${id} - ${clientTop} - ${rootTop} - ${clientBottom}`)
     }
-
-    // console.log(
-    //   `-- ${entry.target.id} - ${entry.boundingClientRect.top} - ${entry.rootBounds.top} -   ${entry.boundingClientRect.bottom}`
-    // )
-
-    //if (
-    //  entry.boundingClientRect.top < entry.rootBounds.top &&
-    //  entry.boundingClientRect.bottom > entry.rootBounds.top
-    //) {
-    //  console.log('a')
-    //  //window.currentId.innerHTML = entry.target.id
-    //} else if (
-    //  entry.boundingClientRect.top < entry.rootBounds.top &&
-    //  entry.boundingClientRect.bottom > entry.rootBounds.top
-    //) {
-    //  console.log('b')
-    //  // window.currentId.innerHTML = entry.target.nextElementSibling.id
-    //}
   })
 }
 
 function createObservers() {
   const options1 = {
     root: null,
-    rootMargin: '-100px',
-    threshold: 1.0,
+    rootMargin: '-200px',
+    threshold: 0,
   }
   const options2 = {
     root: null,
-    rootMargin: '-100px',
-    threshold: 0,
+    rootMargin: '-200px',
+    threshold: 1,
   }
   const observer1 = new IntersectionObserver(handleIntersect1, options1)
   const observer2 = new IntersectionObserver(handleIntersect2, options2)
