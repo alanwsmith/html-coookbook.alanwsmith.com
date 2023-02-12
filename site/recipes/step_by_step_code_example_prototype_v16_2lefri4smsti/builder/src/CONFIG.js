b63b8cc31392c29ed99741fa41211592979820a7
Alan Smith <commit@alanwsmith.com>
2023-02-12 17:26:36 -0500

    Starting to update v16

--- site/recipes/step_by_step_code_example_prototype_v16_2lefri4smsti/builder/src/CONFIG.js
+++ site/recipes/step_by_step_code_example_prototype_v16_2lefri4smsti/builder/src/CONFIG.js
@@ -3,5 +3,127 @@
 // of the main script
 
 const c = {
-  alfa: 'bravo',
+  fadeColor: '#667',
+  source: `fn main() {
+  println!(
+    "main got {}", widget(String::from("apple"))
+  );
+}
+
+fn widget(value: String) -> String {
+  println!("widget got {}", value);
+  String::from("berry")
+}`,
+  sets: [
+    {
+      header: `Goal`,
+      lines: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+      highlights: [],
+      note: `
+      <p>Full code example. Scroll down for step by step details</p>
+      `,
+      noteCoords: [1, 40],
+    },
+
+    {
+      header: `Create The <code>widget</code> Function`,
+      lines: [7, 10],
+      highlights: [7, 10],
+      overrides: [{ line: 7, text: `fn widget() {` }],
+      note: `
+      <p>Start by stubbing out a basic <code>widget</code> fuction. This is what the main process will send data to and receive data from.</p>
+      `,
+      noteCoords: [3, 0],
+    },
+
+    {
+      header: `Accept An Argument`,
+      lines: [7, 10],
+      highlights: [7, 10],
+      overrides: [{ line: 7, text: `fn widget(value: String) {` }],
+      note: `
+      <p>Accept a <code>String</code> that gets bound to <code>value</code></p>
+`,
+      noteCoords: [3, 0],
+    },
+
+    {
+      header: `Add A Return Type`,
+      lines: [7, 10],
+      highlights: [7, 10],
+      overrides: [{ line: 7, text: `fn widget(value: String) -> String {` }],
+      note: `
+      <p>Add a <code>-&gt; String</code> for the return type </p>
+`,
+      noteCoords: [3, 0],
+    },
+
+    {
+      header: `Show Some Output`,
+      lines: [7, 8, 10],
+      highlights: [8],
+      note: `
+      <p>Add a <code>println!()</code> to show the value that gets passed in</p>
+`,
+      noteCoords: [3, 0],
+    },
+
+    {
+      header: `Return Some Data`,
+      lines: [7, 8, 9, 10],
+      highlights: [9],
+      note: `
+      <p>Add a <code>String</code> to return</p>
+`,
+      noteCoords: [3, 0],
+    },
+
+    {
+      header: `Stub The <code>main</code> Function`,
+      lines: [1, 5, 7, 8, 9, 10, 11],
+      highlights: [1, 5],
+      overrides: [],
+      noteCoords: [1, 40],
+      note: `
+      <p>Start making the <code>main</code> function</p>
+
+`,
+    },
+
+    {
+      header: `Stub A <code>println!()`,
+      lines: [1, 2, 3, 4, 5, 7, 8, 9, 10, 11],
+      highlights: [2, 3, 4],
+      overrides: [{ line: 3, text: '    "main got {}"' }],
+      noteCoords: [1, 40],
+      note: `
+      <p>Stub out a <code>println!()</code> with  
+      a format string</p>
+`,
+    },
+
+    {
+      header: `Call <code>widget</code>`,
+      lines: [1, 2, 3, 4, 5, 7, 8, 9, 10, 11],
+      highlights: [3],
+      overrides: [],
+      noteCoords: [1, 40],
+      fades: [{ line: 3, spans: [1, 2, 3, 4] }],
+      note: `
+<p>Make a call to <code>widget</code> passing in a <code>String</code></p>
+`,
+    },
+
+    {
+      header: `Check The Output`,
+      lines: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+      highlights: [],
+      overrides: [],
+      note: `<p>The output from the program is:</p>
+      <pre><code>widget got apple
+main got berry</code></pre>
+      `,
+      noteCoords: [1, 40],
+    },
+  ],
 }
--- site/recipes/step_by_step_code_example_prototype_v16_2lefri4smsti/builder/src/JAVASCRIPT.js
+++ site/recipes/step_by_step_code_example_prototype_v16_2lefri4smsti/builder/src/JAVASCRIPT.js
@@ -1,6 +1,72 @@
+const makeElement = (
+  _type,
+  _id,
+  _html,
+  _childOf,
+  _event,
+  _function,
+  _classes
+) => {
+  const newElement = document.createElement(_type)
+  newElement.id = _id
+  newElement.innerHTML = _html
+  window[_childOf].appendChild(newElement)
+  if (_event !== null) {
+    newElement.addEventListener(_event, _function)
+  }
+  if (_classes) {
+    newElement.classList.add(_classes)
+  }
+  return newElement
+}
+
+const makeSections = () => {
+  c.sets.forEach((set, setIndex) => {
+    const section = makeElement(
+      'section',
+      `section${setIndex}`,
+      '',
+      'wrapper',
+      null,
+      null,
+      'codeSection'
+    )
+
+    const header = makeElement(
+      'h3',
+      `sectionHeader${setIndex}`,
+      set.header,
+      `section${setIndex}`,
+      null,
+      null,
+      'codeSectionHeader'
+    )
+
+    const code = makeElement(
+      'div',
+      `sectionCode${setIndex}`,
+      `<pre><code id=codeBlock${setIndex}>x</code></pre>`,
+      `section${setIndex}`,
+      null,
+      null,
+      'codeSectionCode'
+    )
+
+    const note = makeElement(
+      `div`,
+      `sectionNote${setIndex}`,
+      set.note,
+      `section${setIndex}`,
+      null,
+      null,
+      'codeSectionNote'
+    )
+  })
+}
+
 const init = () => {
-  console.log('init')
-  console.log(c.alfa)
+  c.rawLines = c.source.split('\n')
+  makeSections()
 }
 
 document.addEventListener('DOMContentLoaded', init)
