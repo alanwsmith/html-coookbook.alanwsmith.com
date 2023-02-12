a680eaf359e0602600388c7475ed8444b822d6d4
Alan Smith <commit@alanwsmith.com>
2023-02-11 22:19:28 -0500

    starting to move to the full code version

--- site/recipes/step_by_step_code_example_prototype_v13_scroll_event_2lcpt7jgbnza/builder/src/JAVASCRIPT.js
+++ site/recipes/step_by_step_code_example_prototype_v13_scroll_event_2lcpt7jgbnza/builder/src/JAVASCRIPT.js
@@ -2,22 +2,31 @@ const state = {}
 
 const doThing = () => {
   const codeBlock = window.codeBlock
-  const item1 = window.item1
   const bottomOfCodeBlock = codeBlock.offsetHeight + codeBlock.offsetTop
 
+  const activeItems = []
+
   state.items.forEach((item, itemIndex) => {
-    if (item.offsetTop < bottomOfCodeBlock) {
-      item.style.borderLeft = '1px solid red'
-    } else {
+    if (item.offsetTop > bottomOfCodeBlock) {
       item.style.borderLeft = '1px solid green'
+      activeItems.push(itemIndex)
+    } else {
+      item.style.borderLeft = '1px solid red'
     }
   })
-  console.log(bottomOfCodeBlock)
+
+  if (activeItems.length > 0) {
+    console.log(activeItems[0])
+    // window.theCode.innerHTML = c.sets[state.activeItems[0]]
+  }
+
+  // console.log(bottomOfCodeBlock)
 }
 
 const init = () => {
   state.items = document.querySelectorAll('#contentBlock > div')
-  console.log(state)
+  state.lines = c.source.split('\n')
+  doThing()
   document.addEventListener('scroll', doThing)
 }
 
