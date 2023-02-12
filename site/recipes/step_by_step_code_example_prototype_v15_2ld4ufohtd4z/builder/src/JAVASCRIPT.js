0b98247ea5ad6b76b492d002e567288abb43ef3e
Alan Smith <commit@alanwsmith.com>
2023-02-12 03:29:30 -0500

    Found the line trigger

--- site/recipes/step_by_step_code_example_prototype_v15_2ld4ufohtd4z/builder/src/JAVASCRIPT.js
+++ site/recipes/step_by_step_code_example_prototype_v15_2ld4ufohtd4z/builder/src/JAVASCRIPT.js
@@ -17,30 +17,56 @@ const makeElement = (
   if (_classes) {
     newElement.classList.add(_classes)
   }
+
+  return newElement
 }
 
 const makeNotes = () => {
   c.sets.forEach((setData, setIndex) => {
-    makeElement(
-      'div',
-      `set${setIndex}`,
-      setData.note,
-      'contentBlock',
-      null,
-      null
+    c.notes.push(
+      makeElement(
+        'div',
+        `set${setIndex}`,
+        setData.note,
+        'contentBlock',
+        null,
+        null
+      )
     )
   })
 }
 
+const updateCode = () => {
+  const boxTop = window.contentBlock.getBoundingClientRect().top
+  const targetLine = boxTop + 110
+  c.notes.forEach((note) => {
+    const noteTop = note.getBoundingClientRect().top
+    const noteBottom = note.getBoundingClientRect().bottom
+    if (noteTop < targetLine && noteBottom > targetLine) {
+      console.log(note.id)
+      // console.log(noteBottom)
+
+      // console.log(boxTop)
+      // console.log(noteTop)
+    }
+    //console.log(note)
+  })
+  //console.log(window.contentBlock.getBoundingClientRect().top)
+  // console.log(window.set0.getBoundingClientRect().top)
+  // console.log(c.notes)
+}
+
 const updateEverything = () => {
-  console.log('updating')
+  // console.log('updating')
+  updateCode()
 }
 
 const init = () => {
+  c.notes = []
   c.rawLines = c.source.split('\n')
   makeNotes()
   updateEverything()
-  document.addEventListener('scroll', updateEverything)
+  window.contentBlock.addEventListener('scroll', updateEverything)
 }
 
 document.addEventListener('DOMContentLoaded', init)
