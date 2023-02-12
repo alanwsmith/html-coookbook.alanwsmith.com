d8845926bb04485bc38f7c43db2ed13b3216eebf
Alan Smith <commit@alanwsmith.com>
2023-02-11 20:31:00 -0500

    Basic setup working with single observer using isIntersecting

--- site/recipes/step_by_step_code_example_prototype_v12_scrolling_2lbq1ufz2bpv/builder/src/JAVASCRIPT.js
+++ site/recipes/step_by_step_code_example_prototype_v12_scrolling_2lbq1ufz2bpv/builder/src/JAVASCRIPT.js
@@ -4,9 +4,9 @@ function handleIntersect1(entries, observer) {
     const clientTop = entry.boundingClientRect.top
     const rootTop = entry.rootBounds.top
     const clientBottom = entry.boundingClientRect.bottom
-    console.log(`${id} - ${clientTop} - ${rootTop} - ${clientBottom}`)
     if (clientTop < rootTop && clientBottom > rootTop) {
       console.log('scrolling down')
+      console.log(`-- ${id} - ${clientTop} - ${rootTop} - ${clientBottom}`)
       window.currentCode.innerHTML = entry.target.id
     }
   })
@@ -18,31 +18,62 @@ function handleIntersect2(entries, observer) {
     const clientTop = entry.boundingClientRect.top
     const rootTop = entry.rootBounds.top
     const clientBottom = entry.boundingClientRect.bottom
-    console.log(`${id} - ${clientTop} - ${rootTop} - ${clientBottom}`)
     if (clientTop <= rootTop) {
-      const nextId = entry.target.nextElementSibling.id
-      if (nextId) {
+      const nextSibling = entry.target.nextElementSibling
+      if (nextSibling) {
+        const nextId = nextSibling.id
         console.log('scrolling up')
+        console.log(`-- ${id} - ${clientTop} - ${rootTop} - ${clientBottom}`)
         window.currentCode.innerHTML = c.code[nextId]
+        console.log(entry.isIntersecting)
       }
     }
   })
 }
 
-function createObservers() {
+function handleIntersect3(entries, observer) {
+  entries.forEach((entry) => {
+    const id = entry.target.id
+    const clientTop = entry.boundingClientRect.top
+    const rootTop = entry.rootBounds.top
+    const clientBottom = entry.boundingClientRect.bottom
+    if (clientTop <= rootTop) {
+      const theElement = entry.isIntersecting
+        ? entry.target
+        : entry.target.nextElementSibling
+      if (theElement) {
+        console.log(theElement.id)
+        window.currentCode.innerHTML = c.code[theElement.id]
+      }
+
+      // if (entry.isIntersecting) {
+      // } else {
+      //   const nextSibling = entry.target.nextElementSibling
+      //   if (nextSibling) {
+      //     const nextId = nextSibling.id
+      //     console.log('scrolling up')
+      //     console.log(`-- ${id} - ${clientTop} - ${rootTop} - ${clientBottom}`)
+      //     window.currentCode.innerHTML = c.code[nextId]
+      //     console.log(entry.isIntersecting)
+      //   }
+      // }
+    }
+  })
+}
+
+function _createObservers_v1() {
   const options1 = {
     root: null,
     rootMargin: '-200px 0px 200px 0px',
-    threshold: 0,
+    threshold: 0.4,
   }
   const options2 = {
     root: null,
     rootMargin: '-200px 0px 200px 0px',
-    threshold: 1,
+    threshold: 0.6,
   }
   const observer1 = new IntersectionObserver(handleIntersect1, options1)
   const observer2 = new IntersectionObserver(handleIntersect2, options2)
-
   const els = window.contentArea.getElementsByTagName('div')
   for (let i = 0; i < els.length; i++) {
     observer1.observe(els[i])
@@ -50,6 +81,25 @@ function createObservers() {
   }
 }
 
+function createObservers() {
+  const options3 = {
+    root: null,
+    rootMargin: '-200px 0px 200px 0px',
+    threshold: 0.6,
+  }
+  // const options2 = {
+  //   root: null,
+  //   rootMargin: '-200px 0px 200px 0px',
+  //   threshold: 0.6,
+  // }
+  const observer3 = new IntersectionObserver(handleIntersect3, options3)
+  // const observer2 = new IntersectionObserver(handleIntersect2, options2)
+  const els = window.contentArea.getElementsByTagName('div')
+  for (let i = 0; i < els.length; i++) {
+    observer3.observe(els[i])
+    //observer2.observe(els[i])
+  }
+}
 const init = () => {
   window.currentCode.innerHTML = c.code['code1']
   createObservers()
