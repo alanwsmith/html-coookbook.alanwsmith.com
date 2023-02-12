e19f320b77cf1a8f31c3009c40c8b80ee28b7df5
Alan Smith <commit@alanwsmith.com>
2023-02-11 22:39:21 -0500

    Added the notes

--- site/recipes/step_by_step_code_example_prototype_v13_scroll_event_2lcpt7jgbnza/builder/src/JAVASCRIPT.js
+++ site/recipes/step_by_step_code_example_prototype_v13_scroll_event_2lcpt7jgbnza/builder/src/JAVASCRIPT.js
@@ -33,9 +33,44 @@ const doThing = () => {
   // console.log(bottomOfCodeBlock)
 }
 
+const makeElement = (
+  _type,
+  _id,
+  _html,
+  _childOf,
+  _event,
+  _function,
+  _classes
+) => {
+  const newElement = document.createElement(_type)
+  newElement.id = _id
+  newElement.innerHTML = _html
+  window[_childOf].appendChild(newElement)
+  if (_event !== null) {
+    newElement.addEventListener(_event, _function)
+  }
+  if (_classes) {
+    newElement.classList.add(_classes)
+  }
+}
+
+const makeNotes = () => {
+  c.sets.forEach((setData, setIndex) => {
+    makeElement(
+      'div',
+      `set${setIndex}`,
+      setData.note,
+      'contentBlock',
+      null,
+      null
+    )
+  })
+}
+
 const init = () => {
   state.items = document.querySelectorAll('#contentBlock > div')
   state.lines = c.source.split('\n')
+  makeNotes()
   doThing()
   document.addEventListener('scroll', doThing)
 }
