f6cb4caf127b8dbe14cf619ec8f5cc3a86f28a64
Alan Smith <commit@alanwsmith.com>
2023-02-12 02:13:28 -0500

    Starting to put in code lines

--- site/recipes/step_by_step_code_example_prototype_v13_scroll_event_2lcpt7jgbnza/builder/src/JAVASCRIPT.js
+++ site/recipes/step_by_step_code_example_prototype_v13_scroll_event_2lcpt7jgbnza/builder/src/JAVASCRIPT.js
@@ -65,13 +65,87 @@ const makeNotes = () => {
   })
 }
 
+const updateEverything = () => {
+  console.log('update everything')
+  updateNotes()
+  console.log(c)
+}
+
+const updateNotes = () => {
+  const bottomOfCodeBlock =
+    window.codeBlock.offsetHeight + window.codeBlock.offsetTop
+  const triggerPoint = bottomOfCodeBlock + 60
+
+  const notes = document.querySelectorAll('#contentBlock > div')
+
+  for (let n = 0; n < notes.length; n++) {
+    const noteTop = notes[n].offsetTop
+    const noteBottom = notes[n].offsetTop + notes[n].offsetHeight
+
+    console.log(`-- ${noteTop} - ${bottomOfCodeBlock} - ${noteBottom}`)
+    if (noteTop < triggerPoint && noteBottom > triggerPoint) {
+      c.currentHighlight = n
+    }
+  }
+
+  for (let n = 0; n < notes.length; n++) {
+    if (n === c.currentHighlight) {
+      const lineAssembler = c.rawLines.map((line, lineIndex) => {
+        if (c.sets[n].lines.includes(lineIndex + 1)) {
+          return line
+        }
+      })
+      window.theCode.innerHTML = lineAssembler.join('\n')
+      notes[n].style.borderLeft = '1px solid black'
+    } else {
+      notes[n].style.borderLeft = null
+    }
+  }
+
+  console.log(c.currentHighlight)
+
+  //  .forEach((note) => {
+  // console.log(note.offsetTop)
+  // console.log(bottomOfCodeBlock)
+  // const noteBottom = note.offsetTop + note.offsetHeight
+  // console.log(noteBottom)
+  //    const noteTop = note.offsetTop
+
+  // const noteMiddle = Math.floor(note.offsetHeigth / 2)
+  // console.log(`-- ${noteTop} - ${noteMiddle} - ${noteBottom}`)
+
+  //console.log(`-- ${noteTop} - ${bottomOfCodeBlock} - ${noteBottom}`)
+
+  // if (noteTop < triggerPoint && noteBottom < triggerPoint) {
+  //   note.style.borderLeft = null
+  // } else {
+  //   note.style.borderLeft = '1px solid purple'
+  // }
+
+  // if (noteTop < bottomOfCodeBlock && noteBottom < bottomOfCodeBlock) {
+  //   // note.style.border = '1px solid red'
+  // } else if (noteTop < bottomOfCodeBlock && noteBottom >= bottomOfCodeBlock) {
+  //   const firstNumber = bottomOfCodeBlock - noteTop
+  //   // note.style.border = '1px solid green'
+  // } else {
+  //   if (!opacitySetForFirstNote) {
+  //     // opacitySetForFirstNote = true
+  //   } else {
+  //     // note.style.border = '1px solid blue'
+  //   }
+  // }
+
+  // console.log(noteBottom)
+  //   })
+}
+
 const init = () => {
-  // state.lines = c.source.split('\n')
+  c.currentHighlight = 0
+  c.currentCode = ''
   c.rawLines = c.source.split('\n')
   makeNotes()
-  // state.items = document.querySelectorAll('#contentBlock > div')
-  // doThing()
-  // document.addEventListener('scroll', doThing)
+  updateEverything()
+  document.addEventListener('scroll', updateEverything)
 }
 
 document.addEventListener('DOMContentLoaded', init)
